e12ad69cafabcb01a8805f296ef8cfaa42474cc7
Сергей <sergeyruk312@gmail.com>
2022-10-14 21:25:12 +0300

    ready

---
+++ For/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace For // Note: actual namespace depends on the project name.
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[10];
+            int i = 0;
+            string line;
+            Console.WriteLine("Введите 10 чисел, разделённых пробелами:");
+            line = Console.ReadLine();
+            string[] subs = line.Split(' ');
+            foreach (var sub in subs)
+            {
+                int.TryParse(sub, out numbers[i++]);
+            }
+            Array.Sort(numbers);
+            Console.Write(numbers[9]);
+        }
+    }
+}
